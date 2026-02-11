7bb8c04f34ccbc8de8be41e9c7cf79ab1f920af7
etiennegodin <etiennegodin@duck.com>
2026-02-11 00:13:58 -0500

    cleanup, core __init__ fix

---
+++ src/nukekit/core/__init__.py
@@ -0,0 +1,19 @@
+from .assets import Asset, Gizmo, Script
+from .context import Context
+from .installer import Installer
+from .manifest import Manifest
+from .publisher import Publisher
+from .repository import Repository
+from .versioning import Version
+
+__all__ = [
+    "Asset",
+    "Gizmo",
+    "Script",
+    "Context",
+    "Repository",
+    "Publisher",
+    "Installer",
+    "Manifest",
+    "Version",
+]
