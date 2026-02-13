a4ef62ada98e241d9ad62c24db4b9f1a0e5d5dd7
etiennegodin <etiennegodin@duck.com>
2026-02-13 18:00:53 -0500

    temp path in install test

--- test/integration/test_install_worflow.py
+++ test/integration/test_install_worflow.py
@@ -3,7 +3,9 @@ from nukekit.core import Manifest, installer
 
 def test_install_asset(tmp_path, sample_asset, sample_repo):
     local_manifest = Manifest.from_json(tmp_path / "local_manifest.json")
-    assert installer.install_asset_from_repo(sample_repo, local_manifest, sample_asset)
+    assert installer.install_asset_from_repo(
+        sample_repo, local_manifest, sample_asset, destination_path=tmp_path
+    )
     assert (
         sample_asset
         == local_manifest.data["Gizmo"][sample_asset.name][sample_asset.version]
