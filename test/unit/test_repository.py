6274d52d6a4a73b115d344743b179209e564402d
etiennegodin <etiennegodin@duck.com>
2026-02-17 11:40:15 -0500

    fixes on repo tests, more to come

--- test/unit/test_repository.py
+++ test/unit/test_repository.py
@@ -1,26 +1,24 @@
-from nukekit.core import Manifest, Repository
+from nukekit.core import AssetType, Repository
 
 
-def test_repo_creation(sample_config):
-    repo = Repository(sample_config)
+def test_repo_from_config(sample_config):
+    repo = Repository.from_config(sample_config)
+    assert repo
+
+
+def test_repo_creation(tmp_path):
+    repo = Repository(tmp_path, [a for a in AssetType])
     assert repo
 
 
 def test_repo_folder_creation(sample_config):
-    repo = Repository(sample_config)
-    folder = repo.ROOT / "Gizmo"
+    repo = Repository.from_config(sample_config)
+    folder = repo.root / "Gizmo"
     assert folder.exists()
     assert folder.is_dir()
 
 
-def test_repo_add_manifest(tmp_path, sample_config):
-    repo = Repository(sample_config)
-    manifest = Manifest.from_json(tmp_path / "tmp.json")
-    repo.add_manifest(manifest)
-    assert repo.manifest == manifest
-
-
 def test_repo_build_asset_path(sample_config, sample_asset):
-    repo = Repository(sample_config)
-    path = repo.build_asset_path(sample_asset)
-    assert path == repo.ROOT / "Gizmo" / "tool" / f"{sample_asset}.gizmo"
+    repo = Repository.from_config(sample_config)
+    path = repo.get_asset_path(sample_asset)
+    assert path == repo.root / "Gizmo" / "tool" / f"{sample_asset}.gizmo"
